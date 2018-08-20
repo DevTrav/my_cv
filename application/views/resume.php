12fe83585bc8e2612d98eed6739880febe079975
travers <travers@localhost.localdomain>
2018-08-20 16:25:50 -0500

    added employement history to resume

--- application/views/resume.php
+++ application/views/resume.php
@@ -1,170 +1,187 @@
 <section id="resume" class="container section">
     <div class="row">
-      <div class="col-md-10">
-        <h2 id="resume_header" class="section__title">Resume_</h2>
-        <p class="section__description">
-          Hard-working, <i><b>self-motivated</b></i>,  <i><b>creative</b></i> looking for an enriching Entry Level Web Developer position  to support a team in implementing bar-none user interaction following client-approved mock-ups, such as wireframes, visual design comprehensive layouts, and prototypes. 
-          Bringing a <i><b>keen eye</i></b> for details, strong ability to implement mock-ups, and understanding of great user experience design; <i><b>three years experience</i></b> in visual <i><b>UI / front-end development work, JavaScript, CSS,  JQuery and XHTML 1.1, Web Standard HTML5</i></b> and over 4 years of client service experience.
-        </p>
-      </div>
+        <div class="col-md-10">
+            <h2 id="resume_header" class="section__title">Resume_</h2>
+            <p class="section__description">
+                Hard-working, <i><b>self-motivated</b></i>,  <i><b>creative</b></i> looking for an enriching Entry Level Web Developer position  to support a team in implementing bar-none user interaction following client-approved mock-ups, such as wireframes, visual design comprehensive layouts, and prototypes. 
+                Bringing a <i><b>keen eye</i></b> for details, strong ability to implement mock-ups, and understanding of great user experience design; <i><b>three years experience</i></b> in visual <i><b>UI / front-end development work, JavaScript, CSS,  JQuery and XHTML 1.1, Web Standard HTML5</i></b> and over 4 years of client service experience.
+            </p>
+        </div>
     </div>
     <div class="row">
-      <div class="col-md-8 section__resume resume-list">
-        <h3 class="resume-list_title">education</h3>
-        <div class="resume-list__block">
-          <p class="resume-list__block-title">Concordia University Texas</p>
-          <p class="resume-list__block-date">2016-Present</p>
-          <p>
-            Working toward a Bachelors of Science in Computer Science
-          </p>
+        <div class="col-md-8 section__resume resume-list">
+            <h3 class="resume-list_title">education</h3>
+            <div class="resume-list__block">
+                <p class="resume-list__block-title">Concordia University Texas</p>
+                <p class="resume-list__block-date">2016-Present</p>
+                <p>
+                    Working toward a Bachelors of Science in Computer Science
+                </p>
+            </div>
+            <div class="resume-list__block">
+                <p class="resume-list__block-title">Austin Community College</p>
+                <p class="resume-list__block-date">2015 - 2016</p>
+                <p>
+                    Course work in Python and computer science 
+                </p>
+            </div>
+            <div class="resume-list__block">
+                <p class="resume-list__block-title">Lee College</p>
+                <p class="resume-list__block-date">2003 - 2006</p>
+                <p>
+                    Completed core curriculum 
+                </p>
+            </div>
         </div>
-        <div class="resume-list__block">
-          <p class="resume-list__block-title">Austin Community College</p>
-          <p class="resume-list__block-date">2015 - 2016</p>
-          <p>
-            Course work in Python and computer science 
-          </p>
-        </div>
-        <div class="resume-list__block">
-          <p class="resume-list__block-title">Lee College</p>
-          <p class="resume-list__block-date">2003 - 2006</p>
-          <p>
-            Completed core curriculum 
-          </p>
-        </div>
-      </div>
     </div>
     <div class="row">
-      <div class="col-md-8 section__resume resume-list">
-        <h3 class="resume-list_title">employment</h3>
-        <div class="resume-list__block">
-          <p class="resume-list__block-title">Brandmuscle, Inc.- Account Executive</p>
-          <p class="resume-list__block-date">2013 - 2017</p>
-          <ul class="list_disc">
-              <li>Maintaining client / business-to-business relationships via client meetings and conference calls</li>
-              <li>Responsible for resolving financial data discrepancies above $1 million threshold for ongoing projects </li>
-              <li>Responsible for quarterly project scheduling and budget establishment and adherence</li>
-              <li>Coordinate staffing needs for project support while adhering to project launch schedules</li>
-              <li>Negotiated project schedule and budget with Program Director of Farmers </li>
-              <li>Designed and tested 260 hours worth of project design work to enable a end-user reporting tool</li>
-              <li>Formulated comprehensive test plans to implement reporting tool in adherence of launch schedule</li>
-              <li>Managed products on client site with enriched meta-data for optimal UX, HTML, CSS and DNS event log monitoring</li>
-              <li>Developed over 50 complex analytical queries in SQL to create graphic dashboards and reporting for the client to visualize their data in a meaningful way and solve discrepancies between data and web reporting</li>
-              <li>Resolved payment discrepancies and  transactional disputes valued over $300k</li>
-              <li>Analyzed invoicing process for North America to identify efficiencies</li>
-              <li>Managed  weekly meetings with team members across time zones and offices to coordinate projects and reconcile sales data issues</li>
+        <div class="col-md-8 section__resume resume-list">
+            <h3 class="resume-list_title">employment</h3>
+            <div class="resume-list__block">
+                <p class="resume-list__block-title">Brandmuscle, Inc.- Account Executive</p>
+                <p class="resume-list__block-date">2013 - 2017</p>
+                <ul class="list_disc">
+                    <li>Maintaining client / business-to-business relationships via client meetings and conference calls</li>
+                    <li>Responsible for resolving financial data discrepancies above $1 million threshold for ongoing projects </li>
+                    <li>Responsible for quarterly project scheduling and budget establishment and adherence</li>
+                    <li>Coordinate staffing needs for project support while adhering to project launch schedules</li>
+                    <li>Negotiated project schedule and budget with Program Director of Farmers </li>
+                    <li>Designed and tested 260 hours worth of project design work to enable a end-user reporting tool</li>
+                    <li>Formulated comprehensive test plans to implement reporting tool in adherence of launch schedule</li>
+                    <li>Managed products on client site with enriched meta-data for optimal UX, HTML, CSS and DNS event log monitoring</li>
+                    <li>Developed over 50 complex analytical queries in SQL to create graphic dashboards and reporting for the client to visualize their data in a meaningful way and solve discrepancies between data and web reporting</li>
+                    <li>Resolved payment discrepancies and  transactional disputes valued over $300k</li>
+                    <li>Analyzed invoicing process for North America to identify efficiencies</li>
+                    <li>Managed  weekly meetings with team members across time zones and offices to coordinate projects and reconcile sales data issues</li>
 
-          </ul>
-          
-          
-          
-          
+                </ul>
+
+
+
+
+            </div>
+            <div class="resume-list__block">
+                <p class="resume-list__block-title">Just Brakes- Management/Service Writer</p>
+                <p class="resume-list__block-date">20012 - 2013</p>
+                <ul class="list_disc">
+                    <li>Managed price-points for both the company and customer in accordance with corporate guidelines</li>
+                    <li>Recovered over 20k in accounts receivable per month</li> 
+                    <li>Developed corporate SOP regarding legal liability for services provided</li>
+                    <li>Maintained customer  appointment schedule to ensure a timely customer experience</li>
+                    <li>Coordinated inter-office communication regarding sales campaigns and promotional offers</li>
+
+                </ul>
+            </div>
+            <div class="resume-list__block">
+                <p class="resume-list__block-title">Connectivity Source- Service Technician Manager</p>
+                <p class="resume-list__block-date">2008- 2012</p>
+                <ul class="list_disc">
+                    <li>Improved a third-quintile location to a first-quintile store in under two months</li>
+                    <li> Collaborated with more than 8 insurance companies to process approximately 1,000 cell phone claims per month</li>
+                    <li>Created training procedures to on-board more than 20 additional employees </li>
+                    <li> Promoted to Service Technician Manager after 6 weeks </li>
+                    <li> Maintained accurate billing records to process insurance claims</li>
+                    <li> Received Sprint Service Technician certification</li>
+
+                </ul>
+            </div>
+             <div class="resume-list__block">
+                <p class="resume-list__block-title">La Ville Solutions- Owner</p>
+                <p class="resume-list__block-date">Present</p>
+                <ul class="list_disc">
+                    <li> Manufactured a more dynamic web experience for Austin based businesses and artists by leveraging web languages with various solutioning tools</li>
+                    <li> Consultant </li> 
+                
+            </div>
         </div>
-        <div class="resume-list__block">
-          <p class="resume-list__block-title">Just Brakes</p>
-          <p class="resume-list__block-date">2004 - 2005</p>
-          <p>
-            Awesome developer, lorem ipsum dolor sit amet, conse ctetur adipisicing elit, sed do eius-
-            mod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-          </p>
-        </div>
-        <div class="resume-list__block">
-          <p class="resume-list__block-title">Molly’s studio</p>
-          <p class="resume-list__block-date">2003 - 2006</p>
-          <p>
-            Programmer Lorem ipsum dolor sit amet, consecte tur adipisicing elit, sed do eiusmod tempor
-            incididunt ut
-          </p>
-        </div>
-      </div>
     </div>
     <div class="row section__resume progress-list js-progress-list">
-      <div class="col-md-12">
-        <h3 class="progress-list__title">general skills</h3>
-      </div>
-      <div class="col-md-5 mr-auto">
-        <div class="progress-list__skill">
-          <p>
-            <span class="progress-list__skill-title">html5</span>
-            <span class="progress-list__skill-value">80%</span>
-          </p>
-          <div class="progress">
-            <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" >
-            </div>
-          </div>
+        <div class="col-md-12">
+            <h3 class="progress-list__title">general skills</h3>
         </div>
-        <div class="progress-list__skill">
-          <p>
-            <span class="progress-list__skill-title">css3</span>
-            <span class="progress-list__skill-value">70%</span>
-          </p>
-          <div class="progress">
-            <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
+        <div class="col-md-5 mr-auto">
+            <div class="progress-list__skill">
+                <p>
+                    <span class="progress-list__skill-title">html5</span>
+                    <span class="progress-list__skill-value">80%</span>
+                </p>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" >
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-        <div class="progress-list__skill">
-          <p>
-            <span class="progress-list__skill-title">java script</span>
-            <span class="progress-list__skill-value">90%</span>
-          </p>
-          <div class="progress">
-            <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+            <div class="progress-list__skill">
+                <p>
+                    <span class="progress-list__skill-title">css3</span>
+                    <span class="progress-list__skill-value">70%</span>
+                </p>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-        <div class="progress-list__skill">
-          <p>
-            <span class="progress-list__skill-title">jquery</span>
-            <span class="progress-list__skill-value">80%</span>
-          </p>
-          <div class="progress">
-            <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+            <div class="progress-list__skill">
+                <p>
+                    <span class="progress-list__skill-title">java script</span>
+                    <span class="progress-list__skill-value">90%</span>
+                </p>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-5 mr-auto">
-        <div class="progress-list__skill">
-          <p>
-            <span class="progress-list__skill-title">bootstrap 3</span>
-            <span class="progress-list__skill-value">80%</span>
-          </p>
-          <div class="progress">
-            <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+            <div class="progress-list__skill">
+                <p>
+                    <span class="progress-list__skill-title">jquery</span>
+                    <span class="progress-list__skill-value">80%</span>
+                </p>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-        <div class="progress-list__skill">
-          <p>
-            <span class="progress-list__skill-title">grunt</span>
-            <span class="progress-list__skill-value">60%</span>
-          </p>
-          <div class="progress">
-            <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
+        <div class="col-md-5 mr-auto">
+            <div class="progress-list__skill">
+                <p>
+                    <span class="progress-list__skill-title">bootstrap 3</span>
+                    <span class="progress-list__skill-value">80%</span>
+                </p>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-        <div class="progress-list__skill">
-          <p>
-            <span class="progress-list__skill-title">git</span>
-            <span class="progress-list__skill-value">90%</span>
-          </p>
-          <div class="progress">
-            <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+            <div class="progress-list__skill">
+                <p>
+                    <span class="progress-list__skill-title">grunt</span>
+                    <span class="progress-list__skill-value">60%</span>
+                </p>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-        <div class="progress-list__skill">
-          <p>
-            <span class="progress-list__skill-title">adobe photoshop</span>
-            <span class="progress-list__skill-value">80%</span>
-          </p>
-          <div class="progress">
-            <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+            <div class="progress-list__skill">
+                <p>
+                    <span class="progress-list__skill-title">git</span>
+                    <span class="progress-list__skill-value">90%</span>
+                </p>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                    </div>
+                </div>
             </div>
-          </div>
+            <div class="progress-list__skill">
+                <p>
+                    <span class="progress-list__skill-title">adobe photoshop</span>
+                    <span class="progress-list__skill-value">80%</span>
+                </p>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </section>
+</section>
