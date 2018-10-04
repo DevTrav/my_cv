0b3e5eb972196dfc321065570eaea53764f93d87
devtrav <traverslaville@gmail.com>
2018-10-03 20:25:38 -0600

    fixed broken download tag

--- application/views/greeting.php
+++ application/views/greeting.php
@@ -5,7 +5,7 @@
             <p class="section__description">
                 Hard-working,self-motivated,  <em><strong>creative</strong></em> looking for an enriching Entry Level Web Developer position  to support a team in implementing bar-none user interaction following client-approved mock-ups, such as wireframes, visual design comprehensive layouts, and prototypes. 
                 Bringing a <em><strong>keen eye</strong></em> for details, strong ability to implement mock-ups, and understanding of great user experience design; <em><strong>three years experience</strong></em> in visual <em><strong>UI / front-end development work, JavaScript, CSS,  JQuery and XHTML 1.1, Web Standard HTML5</strong></em> and over 4 years of client service experience.</p>
-            <a href="assets/img/traverslaville_resume.pdf" class="section_btn site-btn"download="traverslaville_resume.pdf><img src="assets/img/img_btn_icon.png" alt="">Download CV</a>
+            <a href="assets/img/traverslaville_resume.pdf" class="section_btn site-btn"download="traverslaville_resume.pdf"><img src="assets/img/img_btn_icon.png" alt="">Download CV</a>
         </div>
     </div>
 </section> 
