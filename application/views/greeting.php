4fae1b063397396ce43d1ecef60b8d5a9fbf23b7
TravDev <traverslaville@gmail.com>
2019-04-30 15:28:51 -0500

    updated greeting text

--- application/views/greeting.php
+++ application/views/greeting.php
@@ -3,10 +3,11 @@
         <div class="col-md-10">
             <h2 id="hello_header" class="section__title">Hi_</h2>
             <p class="section__description">
-                Self-motivated, <strong><em>expert level problem solver</em></strong> looking for a challenging position as a developer, deliverying custom web expiriences within the <strong><em>WordPress eco-system</em></strong>. 
-                Expirience in all aspects of WordPress website creation, including design, plug-ins, and implementation. Skilled in creating engaging and interactive websites. 
-                Excel at team projects and leadership. Detail-oriented and knowledgeable in various programming languages. Dedicated to superior customer service at all levels from the first meeting with a client to the website maintenance after launch. 
-                With <strong><em>more than eight years of working experience within the field of SOP (Standard Operating Procedure) and technical documentation</em></strong>, development and  you can rest assured that I would hit the ground running. 
+                Self-motivated, <strong><em>expert level problem solver</em></strong> looking for a challenging position as a developer, delivering custom web expiriences within the <strong><em>WordPress eco-system</em></strong>. 
+                Expirienced in all aspects of <stong><em>WordPress website creation, including design, plug-ins, and implementation.</p>
+                <p class="section__description"> 
+                As a techechnical professional, I am dedicated to superior customer service at all levels from the first meeting with a client to the website maintenance after launch. 
+                With <strong><em>more than eight years of working experience within the field of SOP (Standard Operating Procedure) and technical documentation</em></strong>  you can rest assured that I would hit the ground running. 
                 As an added <strong>bonus,</strong> I'm quite <strong><em>comfortable with most modern web technologies/frameworks</em></strong> and equally comfortable speaking with business-folk, tech-folk and end customers alike.
             </p>
             <a href="assets/img/traverslaville_resume.pdf" class="section_btn site-btn"download="traverslaville_resume.pdf"><img src="assets/img/img_btn_icon.png" alt="">Download CV</a>
