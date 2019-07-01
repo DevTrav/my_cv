87b38faa9fec3df40ddb3023f862a55d5e101aba
TravDev <traverslaville@gmail.com>
2019-07-01 13:43:08 -0500

    updates to application and e-commerce description, image and url

--- application/views/portfolio.php
+++ application/views/portfolio.php
@@ -35,7 +35,47 @@
             <li>Ajax</li>
             <li>PHP</li>
           </ul>
-          <a href="http://www.imageandgenius.com" class="project-card__link">www.imageandgenius.com</a>
+          <a href="http://www.imageandgenius.com" class="project-card__link">Website</a>
+        </div>
+      </div>
+      
+      <div class="row project-card"  data-toggle="modal" data-target="#portfolioModal3" data-portfolio-tag="e-commerce">
+        <div class="col-md-6 col-lg-5 project-card__img">
+        <img class="" src="<?php echo get_asset_path('images','vog_skincare.jpg'); ?>" alt="mud mask">
+      </div>
+        <div class="col-md-6 col-lg-7 project-card__info">
+        <h3 class="project-card__title">Health and Beauty E-Commerce Store</h3>
+        <p class="project-card__description">
+            This e-commerce store was built with cohesive branding, inventory optimization and a "customer-first" online shopping experience in mind. 
+        </p>
+        <p class="project-card__stack">Used stack:</p>
+        <ul class="tags">
+          <li>html5</li>
+          <li>css3</li>
+          <li>PHP</li>
+          <li>Magento</li>
+          <li>JavaScript</li>
+        </ul>
+        <a href="http://www.vogskincare.com/" class="project-card__link">Store</a>
+      </div>
+      </div>
+      <div class="row project-card"  data-toggle="modal" data-target="#portfolioModal4" data-portfolio-tag="apps">
+        <div class="col-md-6 col-lg-5 project-card__img">
+          <img src="<?php echo get_asset_path('images','v60_13.jpg'); ?>" alt="coffee grounds" />
+        </div>
+        <div class="col-md-6 col-lg-7 project-card__info">
+          <h3 class="project-card__title">V60 Pourover Application</h3>
+          <p class="project-card__description">
+          A tool to help dial in a v60 pourover based on the 4:6 brewing method.</p>
+          <p class="project-card__stack">Used stack:</p>
+          <ul class="tags">
+            <li>html5</li>
+            <li>css3</li>
+            <li>PHP</li>
+            <li>Codeigniter</li>
+            <li>JavaScipt</li>
+          </ul>
+          <a href="http://dialv60.lavillesolutions.com" class="project-card__link">Project</a>
         </div>
       </div>
       <div class="row project-card"  data-toggle="modal" data-target="#portfolioModal2" data-portfolio-tag="landing-pages">
@@ -57,25 +97,4 @@
           <a href="" class="project-card__link">TBD</a>
         </div>
       </div>
-      <div class="row project-card"  data-toggle="modal" data-target="#portfolioModal3" data-portfolio-tag="e-commerce">
-        <div class="col-md-6 col-lg-5 project-card__img">
-        <img class="" src="<?php echo get_asset_path('images','coming_soon_img940.jpg'); ?>" alt="coming soon">
-      </div>
-        <div class="col-md-6 col-lg-7 project-card__info">
-        <h3 class="project-card__title">Health and Beauty E-Commerce Store</h3>
-        <p class="project-card__description">
-            This e-commerce store was built with cohesive branding, inventory optimization and a "customer-first" online shopping experience in mind. 
-        </p>
-        <p class="project-card__stack">Used stack:</p>
-        <ul class="tags">
-          <li>html5</li>
-          <li>css3</li>
-          <li>PHP</li>
-          <li>Magento</li>
-          <li>JavaScript</li>
-        </ul>
-        <a href="" class="project-card__link">TBD</a>
-      </div>
-      </div>
-    </div>
-  </section>
+    </section>
