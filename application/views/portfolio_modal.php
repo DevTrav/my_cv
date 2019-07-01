1ecde8ce92508bb9750439de0240dd55ef554a97
TravDev <traverslaville@gmail.com>
2019-07-01 14:15:43 -0500

    added new modal window for v60 app

--- application/views/portfolio_modal.php
+++ application/views/portfolio_modal.php
@@ -14,7 +14,7 @@
 
         </p>
         <div class="portfolio-modal__link">
-          <a href="http://www.imageandgenius.com">www.imageandgenius.com</a>
+          <a href="http://www.imageandgenius.com">Website</a>
         </div>
         <div  class="portfolio-modal__stack">
           <p class="portfolio-modal__stack-title">Using stack:</p>
@@ -72,13 +72,13 @@
       </div>
       <div class="modal-body col-md-11 col-lg-9 ml-auto mr-auto">
         <p class="portfolio-modal__title">Health And Beauty E-Commerce Store</p>
-        <img class="portfolio-modal__img" src="<?php echo get_asset_path('images','coming_soon_img940.jpg'); ?>" alt="coming soon" />
+        <img class="portfolio-modal__img" src="<?php echo get_asset_path('images','vog_skincare.jpg'); ?>" alt="coming soon" />
         <p class="portfolio-modal__description">
             This e-commerce store was built with cohesive branding, inventory optimization and a "customer-first" online shopping experience in mind. 
 
         </p>
         <div class="portfolio-modal__link">
-          <a href="">TBD</a>
+          <a href="http://www.vogskincare.com/">Store</a>
         </div>
         <div  class="portfolio-modal__stack">
           <p class="portfolio-modal__stack-title">Using stack:</p>
@@ -87,6 +87,38 @@
             <li>css3</li>
             <li>JavaScript</li>
             <li>Magento</li>
+            <li>PHP</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade portfolio-modal" id="portfolioModal4" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body col-md-11 col-lg-9 ml-auto mr-auto">
+        <p class="portfolio-modal__title">Dial V60</p>
+        <img class="portfolio-modal__img" src="<?php echo get_asset_path('images','v60_13.jpg'); ?>" alt="coffee grounds" />
+        <p class="portfolio-modal__description">
+        A tool to help dial in a v60 pourover based on the 4:6 brewing method.</p>
+
+        </p>
+        <div class="portfolio-modal__link">
+          <a href="http://dialv60.lavillesolutions.com">Project</a>
+        </div>
+        <div  class="portfolio-modal__stack">
+          <p class="portfolio-modal__stack-title">Using stack:</p>
+          <ul class="tags">
+            <li>html5</li>
+            <li>css3</li>
+            <li>JavaScript</li>
+            <li>Codeigniter</li>
             <li>PHP</li>
           </ul>
         </div>
