48807f909d10a5c9a439155e438cab392ad3e3a3
DevTrav <traverslaville@gmail.com>
2020-04-19 15:38:53 -0500

    disabled contact form

--- application/controllers/Form.php
+++ application/controllers/Form.php
@@ -10,53 +10,6 @@ class Form extends CI_Controller {
      */
     public function contact()
     {
-        $contact_name = $this->input->post('contact_name');
-        $contact_email = $this->input->post('contact_email');
-        $contact_subject = $this->input->post('contact_subject');
-        $contact_message = $this->input->post('contact_message');
-        //$contact_validate = $this->input->post('contact_validate');
-        $contact_to = get_email_address();
-        
-        if (!$contact_name || !$contact_email || !$contact_message) {
-            
-            //set flash data for error/success messages
-            //$this->session->set_flashdata('message', '<style>div.banner-hide {display:none;}</style><div class="banner-text"><h1 class="responsive-headline">Hmm. Try Again?</h1><br /><h3>Psssst. Check your math.</h3></div>');
-            
-            //redirect back to home page and display flash message
-            //redirect();
-            die("Something went wrong");
-            
-        } else {
-            
-            //host gator does not support mail or SMTP, only sendmail
-            //manually enforce this config
-            $config['protocol'] = 'sendmail';
-            
-            //load email library
-            $this->load->library('email');
-            
-            //init library parameters
-            $this->email->initialize($config);
-            $this->email->set_newline("\r\n");
-            
-            //the from must be an existing email address on the host server
-            $this->email->from('tlaville@gator4269.hostgator.com', 'Contact Form');
-            $this->email->to($contact_to); 
-            $this->email->reply_to($contact_email, $contact_name);
-
-            $this->email->subject($contact_subject);
-            $this->email->message($contact_message); 
-            
-            //send the email
-            $this->email->send();
- 
-            //set flash data for error/success 
-            //$this->session->set_flashdata('message', '<style>div.banner-hide {display:none;}</style><div class="banner-text"><h1 class="responsive-headline">Got Your Message!</h1><br /><h3>You will hear from me shortly.</h3></div>');
-            
-            //redirect back to home page and display flash message
-            //redirect();
-            $message = 'E-Mail Was Sent. Thank You. Click <a href="http://lavillesolutions.com">here</a> to return home.';
-            die($message);
-            }
+        //Add contact form functionality 
     }
 }
